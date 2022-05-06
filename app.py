3f47e8b9a6c15c69635a6acdf02b6b33e40f359a
Me <liorasher79@gmail.com>
2022-05-06 12:30:46 +0300

    added new methods and user credentials parsing

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 #! /usr/bin/python3
 
+import json
 import requests
+import argparse
 from secrets import USER, PASSWORD, SERVER
 
 # -------------------------------------------
@@ -14,16 +16,27 @@ from secrets import USER, PASSWORD, SERVER
 # -------------------------------------------
 
 class CLI:
-    def __init__(self) -> None:
-        self.user = USER
-        self.password = PASSWORD
+    def __init__(self) -> None:        
+        self.username = USER # TODO: replace 'USER' with '' after testing
+        self.password = PASSWORD # TODO: replace 'PASSWORD' with '' after testing
         self.server = SERVER
         self.group = 'administrators'
         self.url = f'https://{self.server}.jfrog.io/artifactory/api/'
+        # self._user_credentials() # TODO: activate this method after testing
         self.token = self._get_token_for_group(self.group)
         self.session = requests.Session()
         self._set_session(self.token)
         # self.menu()
+
+    def _user_credentials(self):
+        # Get user credentials from the command line on script run
+        parser = argparse.ArgumentParser(description='CLI for the JFrog REST API')
+        parser.add_argument('-u', '--username', help="personal username to login to JFrog's REST API", required=True)
+        parser.add_argument('-p', '--password', help="personal password to login to JFrog's REST API", required=True)
+        args = parser.parse_args()
+        # args, unknown = parser.parse_known_args()
+        self.username = args.username
+        self.password = args.password
 
     #----- Utility methods -----
     def _set_session(self, token: str) -> None:
@@ -40,9 +53,9 @@ class CLI:
         endpoint = 'security/token'
         # url = ''.join([self.url, endpoint])
         url = self._set_url(endpoint)
-        data = {'username':self.user, 'password':self.password, 'scope=member-of-groups':group}
-        r = requests.post(url, auth=(self.user, self.password), data=data)
-        # r = self.session.post(url, auth=(self.user, self.password), data=self.data)
+        data = {'username':self.username, 'password':self.password, 'scope=member-of-groups':group}
+        r = requests.post(url, auth=(self.username, self.password), data=data)
+        # r = self.session.post(url, auth=(self.username, self.password), data=self.data)
         return r.json()['access_token']
 
     #----- Menu options implementation -----
@@ -61,6 +74,52 @@ class CLI:
         url = self._set_url(endpoint)    
         r = self.session.get(url, headers=self.session.headers)
         return r.json()['version']
+
+    # 3
+    def _create_user(self) -> requests.status_codes:
+        # Notes: Requires Artifactory Pro
+        # Usage: PUT /api/security/users/{userName}
+        username = input("Enter username: ")
+        email = input("Enter email: ")
+        password = input("Enter password: ")
+        endpoint = f'security/users/{username}'
+        url = self._set_url(endpoint)
+        # email and password are mandatory, name is optional
+        json = {'name': f'{username}',
+                'email' : f'{email}',
+                'password': f'{password}'
+                }
+
+        r = self.session.put(url, headers=self.session.headers, json=json)
+        return r.status_code
+
+    # 4 
+    def _delete_user(self) -> str:
+        # Notes: Requires Artifactory Pro
+        # Usage: DELETE /api/security/users/{userName}
+        username = input("Enter username to delete: ")
+        endpoint = f'security/users/{username}'
+        url = self._set_url(endpoint)
+        msg = self.session.delete(url, headers=self.session.headers)
+        return msg
+
+    # 5
+    def _get_storage_info(self) -> json:
+        # Returns storage summary information regarding binaries, file store and repositories.
+        # Usage: GET /api/storageinfo
+        endpoint = f'/storageinfo'
+        url = self._set_url(endpoint)
+        r = self.session.get(url, headers=self.session.headers)
+        return r.json()
+
+    # 6
+    def _create_repository(self):
+        # Creates a new repository in Artifactory with the provided configuration. Supported by local, remote, virtual and federated repositories. 
+        # Usage: PUT /api/repositories/{repoKey}
+        endpoint = f'/repositories/{repoKey}'
+        url = self._set_url(endpoint)
+        r = self.session.put(url, headers=self.session.headers)
+        pass
 
     # 9
     def _change_group(self, group: str) -> str:
@@ -82,8 +141,11 @@ class CLI:
             12: "Print Token"
             }
 
-        for option in menu_options.keys():            
-            print(option, "--", menu_options[option])
+        for option in menu_options.keys():
+            if option < 10:     
+                print(option, " --", menu_options[option])
+            else:
+                print(option, "--", menu_options[option])
 
     def menu(self) -> None:
         while True:
@@ -106,13 +168,19 @@ class CLI:
                 print(f"Artifactory version {version}")
             elif option == 3:
                 # Create User
-                break
+                status = self._create_user()
+                if status == 200:
+                    print("User created successfully")
+                else:
+                    print("User was not created, try again")
             elif option == 4:
                 # Delete User
-                break
+                msg = self._delete_user()
+                print(msg)
             elif option == 5:
                 # Get Storage Info
-                break
+                info = self._get_storage_info()
+                print(info)
             elif option == 6:
                 # Create Repository
                 break
@@ -143,8 +211,10 @@ class CLI:
             # print(selection)
 
 if __name__ == '__main__':
-    cli = CLI()
     # token = cli._get_token_for_group("readers")
     # print(token)
+    cli = CLI()
     cli.menu()
+    
+
     
